3bceda2bb01131c247611aeac2ff5db69d218a30
Rishkevych <rishkevychdenis@gmail.com>
2025-04-27 15:04:48 +0300

    MCalc - calculator library

---
+++ mcalc.py
@@ -0,0 +1,14 @@
+def calculate():
+    print(eval(input("nums -> ")))
+
+def plus():
+    print(int(input("number 1 ->")) + int(input("number 2 ->")))
+
+def minus():
+    print(int(input("number 1 ->")) - int(input("number 2 ->")))
+
+def divide():
+    print(int(input("number 1 ->")) / int(input("number 2 ->")))
+
+def multiple():
+    print(int(input("number 1 ->")) * int(input("number 2 ->")))
